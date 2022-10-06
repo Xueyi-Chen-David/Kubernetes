b4e4b4ae3ad7fb0163dcb9d721a3509f22fefe2f
David Chen <davidchen8711@gmail.com>
2022-10-06 21:26:34 +0800

    all backup command

---
+++ etcd_backup/list_member.sh
@@ -0,0 +1,4 @@
+sudo ETCDCTL_API=3 etcdctl member list --endpoints https://10.0.2.15:2379 \
+--cert=/etc/kubernetes/pki/etcd/server.crt \
+--key=/etc/kubernetes/pki/etcd/server.key \
+--cacert=/etc/kubernetes/pki/etcd/ca.crt
