68e5c1cec60f27ded6520fb10f9471a377cc42a6
David Chen <davidchen8711@gmail.com>
2022-10-12 23:27:41 +0800

    new file

--- backup.sh
+++
@@ -1,4 +0,0 @@
-ETCDCTL_API=3 etcdctl member list --endpoints https://10.0.2.15:2379 \
---cert=/etc/kubernetes/pki/etcd/server.crt \
---key=/etc/kubernetes/pki/etcd/server.key \
---cacert=/etc/kubernetes/pki/etcd/ca.crt
